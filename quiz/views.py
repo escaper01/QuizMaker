3961c0fcaa1428491cfd632ab2b039d6e1793c1f
escaper <assaoui54@gmail.com>
2020-09-28 23:37:28 +0100

    login - pagination - search => implemented

--- quiz/views.py
+++ quiz/views.py
@@ -5,6 +5,12 @@ from django.views.generic import CreateView, UpdateView, DeleteView
 from django.shortcuts import redirect
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
+from dateutil import parser
+from datetime import datetime
+
+NUM_OF_POSTS = 10
+
 # Create your views here.
 
 def index(request):
@@ -47,26 +53,42 @@ def start_quiz(request,test_name):
         'quiz':quiz,
         'questions':questions
     }
-    #quiz = quiz.objects.filter()
     return render(request,'quiz/start_quiz.html',dataQuiz)
 
 def end_quiz(request):
     return render(request,'quiz/end_quiz.html')
 
 def fill_info(request):
-
-    #print(dir(request))
     if request.POST:
-        #print(request.POST)
         request.session['first_name'] = request.POST.get('first_name')
         request.session['last_name'] = request.POST.get('last_name')
         test_name = request.POST.get('quiz')
         return redirect(reverse('start_quiz',args=[test_name]))
     return render(request,'quiz/fill_info.html')
 
+
+
 @login_required(login_url='login')
 def grades(request):
-    students = Student.objects.all()
+    students_ls = Student.objects.all().order_by('-date')
+    if request.POST:
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        date = request.POST.get('date')
+        if date == '':
+            students_ls = Student.objects.filter(first_name__icontains=first_name,
+                                            last_name__icontains=last_name,
+                                            date__date__lte=datetime.now()).order_by('-date')
+        else:
+            students_ls = Student.objects.filter(first_name__icontains=first_name,
+                                            last_name__icontains=last_name,
+                                            date__date=parser.parse(date)).order_by('-date')
+
+    paginator = Paginator(students_ls, NUM_OF_POSTS)  # Show NUM_OF_PAGES posts per page
+    page = request.GET.get('page')
+
+    students = paginator.get_page(page)
+
     context = {
         'students':students
     }
